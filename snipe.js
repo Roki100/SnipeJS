822daed667c86d8cbacafd2e55a1db5ea1aa15c7
Virachoca <dragoneyedevelopment@gmail.com>
2020-06-09 23:19:19 +0200

    Visual fix

--- snipe.js
+++ snipe.js
@@ -15,7 +15,7 @@ const setTimer = async () => {
     let time = pastNames.data[pastNames.data.length-1].changedToAt + 3196800000;
     if(time - new Date() < 0) return console.log("Failed! Name is taken.");
     setTimeout(snipe, time - new Date())
-    console.log("Sucess! Sniping "+name+" in "+time+"ms")
+    console.log("Sucess! Sniping "+name+" in "+(time - new Date())+"ms")
 };
 
 util.printTitle();
