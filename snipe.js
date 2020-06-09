4573a7b2f1e78a0b2b3fa4d413f1229c96974e13
Virachoca <dragoneyedevelopment@gmail.com>
2020-06-09 23:07:11 +0200

    error handling

--- snipe.js
+++ snipe.js
@@ -8,10 +8,13 @@ const snipe = () => {
 
 const setTimer = async () => {
     let uuidReq = await axios.get("https://api.mojang.com/users/profiles/minecraft/"+args[0]);
+    if(uuidReq.status != 200) return console.log("Failed! Error "+uuidReq.status+": "+uuidReq.statusText);
     let pastNames = await axios.get("https://api.mojang.com/user/profiles/"+uuidReq.data.id+"/names");
+    if(pastNames.data.length == 1) return console.log("Failed! Name is taken.");
     let name = pastNames.data[pastNames.data.length-2].name
-    let time = pastNames.data[pastNames.data.length-1].changedToAt + 3196800000 - new Date();
-    setTimeout(snipe, time)
+    let time = pastNames.data[pastNames.data.length-1].changedToAt + 3196800000;
+    if(time - new Date() < 0) return console.log("Failed! Name is taken.");
+    setTimeout(snipe, time - new Date())
     console.log("Sucess! Sniping "+name+" in "+time+"ms")
 };
 
